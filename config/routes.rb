b568b1f3fc7ea9c0d2e3f3ad6682979b6e38a941
Sally Hall <sally.hall@duke.edu>
2016-11-29 10:25:53 -0500

    add codeon route, nav link

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
     resources :topics, concerns: :commentable, shallow: true
   end
   resources :mentorship, only: [:index]
+  resources :codeon, only: [:index]
 end
